cd59e4f0ed11fda8ae4e879cdfbf88e164b7ab28
Vitalis Kirui <vitaliskirui1@gmail.com>
2022-09-01 14:44:32 +0300

    creating an about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,10 @@ import { GoalDetailComponent } from './Components/goal-detail/goal-detail.compon
 import { StrikethroughDirective } from './Directives/strikethrough.directive';
 import { GoalFormComponent } from './Components/goal-form/goal-form.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './Components/about/about.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { NavbarComponent } from './Components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { HttpClientModule } from '@angular/common/http'
     GoalDetailComponent,
     StrikethroughDirective,
     GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
